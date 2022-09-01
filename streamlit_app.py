63d611154fd918fccc8e4052f9f32caeda0c97f8
Koeun Lim <35312775+koeunlim@users.noreply.github.com>
2022-09-01 10:02:47 -0700

    Update streamlit_app.py
    Add user interaction - multiple select

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Put a pick list for the fruits
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table
 streamlit.dataframe(my_fruit_list)
